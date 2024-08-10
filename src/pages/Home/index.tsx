f8f169c26bdf0486ba6cfd486f7556b4abfc64fb
balthier-dev <punyawee.lim@gmail.com>
2024-08-11 01:17:49 +0700

    change pic

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -21,7 +21,7 @@ const Home = () => {
         title={IntroContent.title}
         content={IntroContent.text}
         button={IntroContent.button}
-        icon="product.jpg"
+        icon="1.jpeg"
         id="intro"
       />
       {/* <MiddleBlock
@@ -34,21 +34,21 @@ const Home = () => {
         title={AboutContent.title}
         content={AboutContent.text}
         contentsecond={AboutContent.textsecond}
-        icon="1.jfif"
+        icon="br.jpeg"
         id="about"
       />
       <ContentBlock
         direction="right"
         title={MissionContent.title}
         content={MissionContent.text}
-        icon="2.jfif"
+        icon="2.jpeg"
         id="mission"
       />
       <ContentBlock
         direction="left"
         title={ProductContent.title}
         content={ProductContent.text}
-        icon="3.jfif"
+        icon="3.jpeg"
         id="product"
       />
       {/* <Contact
